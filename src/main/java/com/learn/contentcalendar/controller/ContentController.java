cddd21b87fbc113f4a5020ea9e26c37d25f4d06d
Vivek-02 <56870780+Vivek-02@users.noreply.github.com>
2024-05-14 21:28:31 +0530

    updating findById logic

--- src/main/java/com/learn/contentcalendar/controller/ContentController.java
+++ src/main/java/com/learn/contentcalendar/controller/ContentController.java
@@ -2,9 +2,12 @@ package com.learn.contentcalendar.controller;
 
 import com.learn.contentcalendar.model.Content;
 import com.learn.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,5 +25,11 @@ public class ContentController {
     @GetMapping("")
     public List<Content> findAllContents() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findContentById(@PathVariable Integer id) {
+        return repository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Content not found"));
     }
 }
